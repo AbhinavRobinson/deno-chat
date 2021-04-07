bf451cb27c2b4d9ac8908e075c11ed162a9e224a
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-07 17:21:31 +0530

    (partial): join to chat

--- chat.js
+++ chat.js
@@ -1,12 +1,28 @@
 import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
+/**
+ * UserID
+ *  > name: string
+ *  > groupName: string,
+ *  > ws: WebSocket
+ */
+const usersMap = new Map();
+
+/**
+ * GroupName: [user1, user2]
+ *  > userId: string,
+ *  > name: string,
+ *  > ws: WebSocket 
+ */
+const groupsMap = new Map();
+
 const chat = async (ws) => {
   console.log("Chat Connected");
 
   const userId = v4.generate();
 
   for await (const data of ws) {
-    console.log(data);
+    const event = JSON.parse(data);
   }
 };
 
